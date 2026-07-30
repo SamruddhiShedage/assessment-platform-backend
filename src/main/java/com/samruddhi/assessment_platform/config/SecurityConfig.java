84724148b345802b102b60d29c76951692addf7e
Your GitHub Username <your-email@example.com>
2026-07-30 23:48:42 +0530

    Fix SecurityConfig

--- src/main/java/com/samruddhi/assessment_platform/config/SecurityConfig.java
+++ src/main/java/com/samruddhi/assessment_platform/config/SecurityConfig.java
@@ -1,28 +1,84 @@
 package com.samruddhi.assessment_platform.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 public class SecurityConfig {
+
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+            .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
+
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    org.springframework.http.HttpMethod.OPTIONS,
+                    "/**"
+                ).permitAll()
+
                 .anyRequest().permitAll()
             );
 
+
         return http.build();
+    }
+
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+
+        configuration.setAllowedOrigins(List.of(
+            "https://assessment-platform-frontend-git-main-sam-4b42.vercel.app"
+        ));
+
+
+        configuration.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ));
+
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+
+        configuration.setAllowCredentials(true);
+
+
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+
+        source.registerCorsConfiguration("/**", configuration);
+
+
+        return source;
     }
 }
